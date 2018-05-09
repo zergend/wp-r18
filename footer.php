b6eb3e4e992e97d01b6c00f4fa5e6c04475edf55
zergend <zergend@gmail.com>
2018-05-09 23:27:41 +0300

    n

--- footer.php
+++ footer.php
@@ -1,5 +1,21 @@
     <footer>
-      <h3>Подвал</h3>
+      <div class="footer--inside">
+        <div class="footer--column-1">
+
+        </div>
+        <div class="footer--column-2">
+
+        </div>
+        <div class="footer--column-3">
+
+        </div>
+        <div class="footer--column-4">
+          <p>391200, Рязанская область,<br>
+          г. Кораблино, ул. Маяковского, д. 30В</p>
+          <p>тел.: +7 (49143) 5-17-75 / 5-17-74</p>
+          <p>e-mail: fskrekord@mail.ru</p>
+        </div>
+      </div>
     </footer>
 
   <script src="<?php echo get_template_directory_uri();
