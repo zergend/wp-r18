ced223e0f8dece2950471b1e83e371d6b31ae2e1
zergend <zergend@gmail.com>
2018-05-16 16:40:56 +0300

    e2

--- template-parts/content-map.php
+++ template-parts/content-map.php
@@ -6,7 +6,7 @@
  */
 ?>
 
-<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ab78a600cce8f2f66a1b6363afa67bad32ac174df93e0a6222315d57014e77f8d&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ab78a600cce8f2f66a1b6363afa67bad32ac174df93e0a6222315d57014e77f8d&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=false"></script>
 
 	
 
