6e86134879acb497553606ef78a8945fa211bbad
zergend <zergend@gmail.com>
2018-05-17 15:52:36 +0300

    d

--- footer.php
+++ footer.php
@@ -13,11 +13,11 @@
         <div class="footer--column-4">
           <p>391200, Рязанская область,<br>
           г. Кораблино, ул. Маяковского, д. 30В</p>
-          <div class='links'>            
-            <span class='vk'><a href="https://vk.com/club165047811" title="Группа ГАУ ДО «ДЮСШ «Рекорд» ВКонтакте"><img src="<?php echo get_template_directory_uri(); ?>/img/vk32.png" alt="ВКонтакте"></a></span>
-            <span class='tel'><a href="tel:+74914351776" title="+7 (49143) 5-17-76"><img src="<?php echo get_template_directory_uri(); ?>/img/tel32.png" alt="Телефон"></a></span>
-            <span class='email'><a href="mailto:fskrekord@mail.ru" title="fskrekord@mail.ru"><img src="<?php echo get_template_directory_uri(); ?>/img/email32.png" alt="Эл.почта"></a></span>
-            <span class='developer'><a href="http://blogsector.ru" title="Разработка и сопровождение"><img src="<?php echo get_template_directory_uri(); ?>/img/developer32.png" alt="Разработчик"></a></span>
+          <div class='links'>
+            <a href="https://vk.com/club165047811" title="Группа ГАУ ДО «ДЮСШ «Рекорд» ВКонтакте"><i class="sprite sprite-vk32"></i></a>
+            <a href="tel:+74914351776" title="+7 (49143) 5-17-76"><i class="sprite sprite-tel32"></i></a>
+            <a href="mailto:fskrekord@mail.ru" title="fskrekord@mail.ru"><i class="sprite sprite-email32"></i></a>
+            <a href="http://blogsector.ru" title="Разработка и сопровождение"><i class="sprite sprite-developer32"></i></a>
           </div>
         </div>
       </div>
