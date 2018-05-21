b081abeb23210c8e96dc3bbe957e91808ab07d69
zergend <zergend@gmail.com>
2018-05-21 17:07:42 +0300

    e

--- footer.php
+++ footer.php
@@ -4,8 +4,7 @@
         <p>&copy; <?php echo date('Y') ?> ГАУ ДО «ДЮСШ «Рекорд»</p>
         <p>При полном или частичном использовании материалов ссылка на официальный сайт <a href="/">ГАУ ДО «ДЮСШ «Рекорд»</a> обязательна.</p>  
         <!-- Yandex.Metrika informer -->
-        <a href="https://metrika.yandex.ru/stat/?id=48947690&amp;from=informer"
-        target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/48947690/3_0_2078C7FF_0058A7FF_1_pageviews"
+        <a href="https://metrika.yandex.ru/stat/?id=48947690&amp;from=informer" class="ym-informer" target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/48947690/3_0_2078C7FF_0058A7FF_1_pageviews"
         style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="48947690" data-lang="ru" /></a>
         <!-- /Yandex.Metrika informer -->
         </div>
